ea7811ac0f4b5d2a21fb29dc5019463147685090
huubsoon <hubertskorulski@gmail.com>
2021-10-16 18:44:57 +0200

    Add heroku configuration

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -1,19 +1,21 @@
 package com.crud.tasks.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class TaskDto {
     private Long id;
     private String title;
     private String content;
 
-    public TaskDto(String title, String content) {
+    /*public TaskDto(String title, String content) {
         this.title = title;
         this.content = content;
-    }
+    }*/
 
 
 
